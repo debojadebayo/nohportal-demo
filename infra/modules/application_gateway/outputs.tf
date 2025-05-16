b99a02edad35fb3cc462741f8405b713d7003225
Debo Adebayo <debojadebayo@gmail.com>
2025-05-16 18:07:20 +0200

    fix:updated fqdn to prevent null

--- infra/modules/application_gateway/outputs.tf
+++ infra/modules/application_gateway/outputs.tf
@@ -1,5 +1,5 @@
 output "gateway_fqdn" {
-  value = azurerm_public_ip.app_gateway_ip.fqdn
+  value = "${azurerm_public_ip.app_gateway_ip.domain_name_label}.${var.location}.cloudapp.azure.com"
 }
 output "gateway_public_ip" {
   value = azurerm_public_ip.app_gateway_ip.ip_address
