85f2c836ef0d2f7b6f2e0c3ee5794c7d0f3cba1d
debojadebayo <debojadebayo@gmail.com>
2025-06-10 13:08:53 +0200

    fix:backend variables adjusted

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg-latest"
-    storage_account_name = "nationohtfstatelatest"
-    container_name       = "tfstatelatest"
+    storage_account_name = "nationohtfstatejun2025tf"
+    container_name       = "tfstate"
     key                  = "nationoh/dev.tfstate.latest"
   }
 }
