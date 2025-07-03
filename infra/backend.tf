ca4d9bb62459a6d587f0e7e6f657bdaaa125c526
debojadebayo <debojadebayo@gmail.com>
2025-07-03 16:23:16 +0100

    stage1: terraform base

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg-latest"
-    storage_account_name = "nationohtfstatejun2025tf"
-    container_name       = "tfstate"
-    key                  = "nationoh/dev.tfstate.latest"
-  }
-}
